5be4a096b2e9b96d187cc9955ee9518f94ef3d4b
jamesmckayfyp <79975138+jamesmckayfyp@users.noreply.github.com>
2021-03-24 15:49:36 +0000

    Welcome Component Added

--- fyp/src/components/Content.vue
+++ fyp/src/components/Content.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="container">
         <Example />
+        <Welcome />
     </div>
 </template>
 
 <script>
 import Example from './Example'
+import Welcome from './Welcome'
 
 export default {
   name: 'Content',
   components: {
-      Example,
+    Example,
+    Welcome,
   }
 }
 </script>
