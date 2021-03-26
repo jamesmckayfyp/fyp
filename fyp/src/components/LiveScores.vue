920ab6e020f36634b4d288e4e4d4784c88a99cee
jamesmckayfyp <79975138+jamesmckayfyp@users.noreply.github.com>
2021-03-26 14:51:41 +0000

    Live scores with vue data added

--- fyp/src/components/LiveScores.vue
+++ fyp/src/components/LiveScores.vue
@@ -2,8 +2,28 @@
     <div class="ls">
         <div class="ls--block">
             <div class="ls--block__heading">Live Scores</div>
-            <div class="ls--block__content">Fernandes 96' (pen)</div>
+            <div class="ls--block__content">
+                <div class="team home">{{homeTeam}}</div>
+                <div class="score">{{homeScore}} - {{awayScore}}</div>
+                <div class="team away">{{awayTeam}}</div>
+            </div>
             <div class="ls--block__cta">View All</div>
         </div>
     </div>
 </template>
+
+<script>
+
+export default {
+  name: 'LiveScores',
+  data() {
+      return {
+        homeTeam: 'Team',
+        awayTeam: 'Team',
+        homeScore: 0,
+        awayScore: 0
+      }
+  }
+}
+
+</script>
