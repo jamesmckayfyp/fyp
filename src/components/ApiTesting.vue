121259cf47445e98b81f5aa49eec0fb77aff00a2
jamesmckayfyp <79975138+jamesmckayfyp@users.noreply.github.com>
2021-03-29 15:59:39 +0100

    API testing files added
    Example league stats pulled through

---
+++ src/components/ApiTesting.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="api-testing">
+    <div>{{ data.competition.name }} Leaders</div>
+    <div>Team: {{ data.standings[0].table[0].team.name }}</div>
+    <div>Points: {{ data.standings[0].table[0].points }}</div>
+    <img :src="data.standings[0].table[0].team.crestUrl" :alt="data.standings[0].table[0].team.name">
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ApiTesting",
+  data() {
+    return {
+      data: {},
+    };
+  },
+  beforeMount() {
+    this.getName();
+  },
+  methods: {
+    async getName() {
+      const res = await fetch(
+        "https://api.football-data.org/v2/competitions/PL/standings"
+      );
+      const data = await res.json();
+      this.data = data;
+    },
+  },
+};
+</script>
--- src/components/Content.vue
+++ src/components/Content.vue
@@ -4,18 +4,21 @@
       <Welcome /> 
       <LiveScores />
     </div>
+    <ApiTesting />
   </div>
 </template>
 
 <script>
 import Welcome from './Welcome'
 import LiveScores from './LiveScores'
+import ApiTesting from './ApiTesting'
 
 export default {
   name: 'Content',
   components: {
     Welcome,
     LiveScores,
+    ApiTesting,
   }
 }
 </script>
