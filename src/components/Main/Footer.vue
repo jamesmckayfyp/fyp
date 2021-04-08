70532e73d44dd305b67d71a1df4ede1c72a241b1
jamesmckayfyp <79975138+jamesmckayfyp@users.noreply.github.com>
2021-04-08 09:54:01 +0100

    Header & Footer fix

--- src/components/Main/Footer.vue
+++ src/components/Main/Footer.vue
@@ -1,23 +1,23 @@
 <template>
-    <div class="footer">
-        <div class="footer__section">
-            statpad 2021 ©
-        </div>
-        <div class="footer__section">
-            Links
-            <ul>
-                <li><a href="#" alt="#">Link</a></li>
-                <li><a href="#" alt="#">Link</a></li>
-                <li><a href="#" alt="#">Link</a></li>
-            </ul>
-        </div>
-        <div class="footer__section">
-            Social
-            <ul>
-                <li><a href="#" alt="#">Link</a></li>
-                <li><a href="#" alt="#">Link</a></li>
-                <li><a href="#" alt="#">Link</a></li>
-            </ul>
-        </div>
+  <div class="footer">
+    <div class="footer--content">
+      <div class="footer--content__section">statpad 2021 ©</div>
+      <div class="footer--content__section">
+        Links
+        <ul>
+          <li><a href="#" alt="#">Link</a></li>
+          <li><a href="#" alt="#">Link</a></li>
+          <li><a href="#" alt="#">Link</a></li>
+        </ul>
+      </div>
+      <div class="footer--content__section">
+        Social
+        <ul>
+          <li><a href="#" alt="#">Link</a></li>
+          <li><a href="#" alt="#">Link</a></li>
+          <li><a href="#" alt="#">Link</a></li>
+        </ul>
+      </div>
     </div>
+  </div>
 </template>
--- src/components/Main/Header.vue
+++ src/components/Main/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header">
-    <div class="header-content">
+    <div class="header--content">
       <div class="logo">
         <router-link to="/">statpad</router-link>
       </div>
