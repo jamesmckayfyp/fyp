88441b0152541c649d9564f44acf7b8d490c6a04
jamesmckayfyp <79975138+jamesmckayfyp@users.noreply.github.com>
2021-04-20 09:57:46 +0100

    Small accessibility update

--- src/components/Main/Navigation.vue
+++ src/components/Main/Navigation.vue
@@ -3,19 +3,19 @@
     <ul>
       <li>
         <router-link to="/Games" alt="Games"
-          ><span class="icon icon-games"></span
+          ><span class="icon icon-games" aria-hidden="true"></span
           ><span class="nav-text">Games</span></router-link
         >
       </li>
       <li>
         <router-link to="/Tables" alt="Tables"
-          ><span class="icon icon-tables"></span
+          ><span class="icon icon-tables" aria-hidden="true"></span
           ><span class="nav-text">Tables</span></router-link
         >
       </li>
       <li>
         <router-link to="/Players" alt="Players"
-          ><span class="icon icon-players"></span
+          ><span class="icon icon-players" aria-hidden="true"></span
           ><span class="nav-text">Players</span></router-link
         >
       </li>
--- src/components/Tables/LeaguePlayerStat.vue
+++ src/components/Tables/LeaguePlayerStat.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="player-stat">
         <div class="player-stat--content">
-            <img class="player-stat--content__img" :src="('https://cdn.soccerwiki.org/images/player/' + playerImage)">
+            <img class="player-stat--content__img" :src="('https://cdn.soccerwiki.org/images/player/' + playerImage)" :alt="playerName">
             <div class="player-stat--content__player">{{playerName}}<span>{{playerClub}}</span></div>
         </div>
         <div class="player-stat--stat">{{statNumber}}</div>
